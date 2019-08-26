8bab0d4377923cd4845aaf25bae4eb8e2cb93950
Kessy Similien <simkdriod@gmail.com>
2019-08-26 07:09:47 -0700

    Implement stacks

---
+++ data_stacks.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  // push - Add value to start of stack
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let oldFirst = this.first;
+      this.first = newNode;
+      this.first.next = oldFirst;
+    }
+
+    return this.size++;
+  }
+
+  // pop - remove from beginning
+  pop() {
+    if (this.size === 0) return null;
+
+    let removed = this.first;
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = removed.next;
+
+    this.size--;
+
+    return removed.val;
+  }
+}
+var stack = new Stack();
