2e5dfa1fb22b1341bd08c99181751a84c0962437
Kessy Similien <simkessy@gmail.com>
2019-09-18 07:07:49 -0700

    BST traversal

--- FCC/bst_trees.js
+++ FCC/bst_trees.js
@@ -39,13 +39,14 @@ class BST {
             return;
           } else if (node.right !== null) {
             return searchTree(node.right);
-          } else {
-            // if data equal - don't add data to tree
-            return null;
           }
         }
+        // data matches
+        else {
+          // if data equal - don't add data to tree
+          return null;
+        }
       };
-
       // start with root and traverse through it
       return searchTree(node);
     }
@@ -76,20 +77,20 @@ class BST {
   find(data) {
     let current = this.root;
 
-    while (current) {
-      if (data === current.data) {
-        return true;
-      }
-
+    while (current.data !== data) {
       if (data < current.data) {
         current = current.left;
       } else {
         current = current.right;
       }
+      if (current === null) {
+        return null;
+      }
     }
 
     return current;
   }
+
   isPresent(data) {
     let current = this.root;
 
@@ -154,30 +155,124 @@ class BST {
   }
 
   findMinHeight(node = this.root) {
-    if (node === null) return -1;
+    if (node === null) return 0;
 
-    let left = this.findMinHeight(node.left);
-    let right = this.findMinHeight(node.right);
+    let left = this.findMaxHeight(node.left);
+    let right = this.findMaxHeight(node.right);
 
-    if (left < right) {
-      return left + 1;
+    return 1 + Math.min(left, right);
+  }
+
+  findMaxHeight(node = this.root) {
+    if (node === null) return 0;
+
+    let left = this.findMaxHeight(node.left);
+    let right = this.findMaxHeight(node.right);
+
+    return 1 + Math.max(left, right);
+  }
+
+  // start from left side
+  inOrder() {
+    if (this.root === null) {
+      return null;
     } else {
-      return right + 1;
+      var result = [];
+
+      let traverseInOrder = node => {
+        // if there's a left, recurse over left side
+        node.left && traverseInOrder(node.left);
+        // push left value to result
+        result.push(node.data);
+        // then push rights
+        node.right && traverseInOrder(node.right);
+      };
+
+      traverseInOrder(this.root);
+      return result;
     }
   }
-}
 
+  // preorder explores root nodes before leaves
+  preOrder() {
+    if (this.root === null) {
+      return null;
+    } else {
+      var result = [];
+
+      let traverseInOrder = node => {
+        // push top
+        result.push(node.data);
+        // if there's a left, recurse over left side
+        node.left && traverseInOrder(node.left);
+        // then recurse over right side
+        node.right && traverseInOrder(node.right);
+      };
+
+      traverseInOrder(this.root);
+      return result;
+    }
+  }
+
+  // post order explores leaf nodes before the root
+  postOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traversePostOrder(node) {
+        node.left && traversePostOrder(node.left);
+        node.right && traversePostOrder(node.right);
+        result.push(node.data);
+      }
+      traversePostOrder(this.root);
+      return result;
+    }
+  }
+
+  // does each row by row
+  levelOrder() {
+    let result = [];
+    let Q = [];
+
+    if (this.root === null) return null;
+
+    Q.push(this.root);
+
+    while (Q.length > 0) {
+      let node = Q.shift();
+      result.push(node.data);
+      if (node.left != null) {
+        Q.push(node.left);
+      }
+      if (node.right != null) {
+        Q.push(node.right);
+      }
+    }
+    return result;
+  }
+}
 const bst = new BST();
+
+bst.add(9);
 bst.add(4);
-bst.add(2);
-bst.add(6);
-bst.add(1);
+bst.add(17);
 bst.add(3);
+bst.add(6);
+bst.add(22);
 bst.add(5);
 bst.add(7);
-bst.remove(4);
-console.log(bst.findMin());
-console.log(bst.findMax());
-bst.remove(7);
-console.log(bst.findMax());
-console.log(bst.isPresent(4));
+bst.add(20);
+
+console.log(bst.findMinHeight());
+console.log(bst.findMaxHeight());
+console.log(bst.isBalanced());
+bst.add(10);
+console.log(bst.findMinHeight());
+console.log(bst.findMaxHeight());
+console.log(bst.isBalanced());
+console.log("inOrder: " + bst.inOrder());
+console.log("preOrder: " + bst.preOrder());
+console.log("postOrder: " + bst.postOrder());
+
+console.log("levelOrder: " + bst.levelOrder());
